ff564564548567f0485831fe84e0dff22116e3f0
AlissonGRN <guilhermeribeiro099@gmail.com>
2020-05-22 21:32:41 -0300

    created routes for get, and post data and getImage midleware

--- models/image.js
+++ models/image.js
@@ -1,17 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	url: {
-		type: String,
-		required: true
-	},
-	postDate: {
-		type: Date,
-		required: true,
-		default: Date.now
-	}
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  postDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+
+module.exports = mongoose.model('Image', imageSchema)
--- routes/images.js
+++ routes/images.js
@@ -1,24 +1,57 @@
-const express = require('express')
-const router = express.Router()
-const Image = require('../models/image')
+const express = require("express");
+const router = express.Router();
+const Image = require("../models/image");
 
 // GET all
-router.get('/', (req, res) => {
-    res.send('Hello')
-})
+router.get("/", async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.json(images);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // GET one
-router.get("/:id", (req, res) => {})
+router.get("/:id", getImage, (req, res) => {
+	res.json(res.image)
+});
 
 // CREATING one
-router.post('/', (req, res) => {})
+router.post("/", async (req, res) => {
+  const image = new Image({
+    name: req.body.name,
+    url: req.body.url,
+    postDate: req.body.postDate,
+	});
+	try {
+		const newImage = await image.save()
+		res.status(201).json(newImage)
+	} catch(error) {
+		res.status(404).json({message: error.message})
+	}
+});
 
 // UPDATE one
-router.patch("/:id", (req, res) => {})
+router.patch("/:id", (req, res) => {});
 
-// DELETE one 
-router.delete('/:id', (req, res) => {})
+// DELETE one
+router.delete("/:id", (req, res) => {});
 
-// getImages midleware
+// getImage midleware
+async function getImage(req, res, next) {
+	let image
+	try {
+		image = await Image.findById(req.params.id)
+		if (image == null) {
+			return res.status(404).send({message: "Cannot find subscriber"})
+		}
+	} catch (error) {
+		return res.status(500).json({message: error.message})
+	}
+
+	res.image = image
+	next()
+}
 
 module.exports = router;
