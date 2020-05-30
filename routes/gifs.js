8234a4ddc5b7c58ef68833c67a479d00053e83a7
AlissonGRN <guilhermeribeiro099@gmail.com>
2020-05-30 20:47:47 -0300

    finished gif routes

--- routes/gifs.js
+++ routes/gifs.js
@@ -1,25 +1,78 @@
 const express = require("express");
 const router = express.Router();
-const Image = require('../models/gif')
+const Image = require("../models/gif");
 
 // GET all
-router.get('/', async (req, res) => {
-    try {
-        const images = await Image.find()
-        res.json(images)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+router.get("/", async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.json(images);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// GET one
+router.get("/:id", getGif, (req, res) => {
+	res.json(res.gif)
 })
 
-// GET one 
-
 // CREATE
+router.post("/", async (req, res) => {
+	const gif = new Gif({
+		name: req.body.name,
+		url: req.body.url,
+		postDate: req.body.postDate,
+	})
+	try {
+		const newGif = await gif.save()
+		res.status(201).json(newGif)
+	} catch (error) {
+		res.status(404).json({ message: error.message })
+	}
+})
 
 // UPDATE
+router.patch("/:id", getGif, async (req, res) => {
+	if (req.body.name != null){
+		res.image.name = req.body.name;
+	}
+	if (req.body.url != null){
+		res.image.url = req.body.url;
+	}
+	try {
+		const updatedGif = await res.gif.save()
+		res.json(updatedGif)
+	} catch (error) {
+		res.status(400).json({ message: error.message })		
+	}
+
+})
 
 // DELETE
+router.delete("/:id", getGif, async (req, res) => {
+	try {
+		await res.image.remove()
+		res.json({message: "Gif deleted"})
+	} catch (error) {
+		res.status(500).json({message: error.message })
+	}
+})
 
 // getGif midleware
+async function getGif(req, res, next) {
+  let gif;
+  try {
+    gif = await Image.findById(req.params.id);
+    if (gif == null) {
+      return res.status(404).send({ message: "Cannot find gif" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.gid = gif;
+  next();
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const db = mongoose.connection;
 
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Conected to Database"));
-
 app.use(express.json());
 
 const imageRouter = require("./routes/images");
